bdd7eefac82ef8cebfa6b241e755d2bae90cdd96
Michel Grootjans <michel.grootjans@gmail.com>
2016-10-22 14:56:53 +0200

    first move

--- spec/langtons-ant-spec.js
+++ spec/langtons-ant-spec.js
@@ -1,8 +1,8 @@
 describe("Langton's ant", function(){
-  var ant = null;
+  var ant, plane = null;
   beforeEach(function() {
-    ant = new Ant();
     plane = new Plane();
+    ant = new Ant(plane);
   });
 
   describe('initially', function() {
@@ -14,6 +14,23 @@ describe("Langton's ant", function(){
     });
     it('starts in a white plane', function() {
       expect(plane.colorAt({x: 0, y: 0})).toEqual('white');
+      expect(plane.colorAt({x: 0, y: 1})).toEqual('white');
+    });
+  });
+
+  describe('after one move', function() {
+    beforeEach(function() {
+      ant.move();
+    });
+    it('moved to (0,1)', function(){
+      expect(ant.position).toEqual({x: 0, y: 1});
+    });
+    it('faces east', function(){
+      expect(ant.direction).toEqual('east');
+    });
+    it('leaves a black square', function() {
+      expect(plane.colorAt({x: 0, y: 0})).toEqual('black');
+      expect(plane.colorAt({x: 0, y: 1})).toEqual('white');
     });
   });
 });
