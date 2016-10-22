759217401484bf91c666cc6a783d08eef51f1e1a
Michel Grootjans <michel.grootjans@gmail.com>
2016-10-22 15:07:50 +0200

    second move

--- spec/langtons-ant-spec.js
+++ spec/langtons-ant-spec.js
@@ -15,6 +15,8 @@ describe("Langton's ant", function(){
     it('starts in a white plane', function() {
       expect(plane.colorAt({x: 0, y: 0})).toEqual('white');
       expect(plane.colorAt({x: 0, y: 1})).toEqual('white');
+      expect(plane.colorAt({x: 1, y: 1})).toEqual('white');
+      expect(plane.colorAt({x: 1, y: 0})).toEqual('white');
     });
   });
 
@@ -31,6 +33,27 @@ describe("Langton's ant", function(){
     it('leaves a black square', function() {
       expect(plane.colorAt({x: 0, y: 0})).toEqual('black');
       expect(plane.colorAt({x: 0, y: 1})).toEqual('white');
+      expect(plane.colorAt({x: 1, y: 1})).toEqual('white');
+      expect(plane.colorAt({x: 1, y: 0})).toEqual('white');
+    });
+  });
+
+  describe('after two moves', function() {
+    beforeEach(function() {
+      ant.move();
+      ant.move();
+    });
+    it('moved to (1,1)', function(){
+      expect(ant.position).toEqual({x: 1, y: 1});
+    });
+    it('faces west', function(){
+      expect(ant.direction).toEqual('south');
+    });
+    it('leaves a black square', function() {
+      expect(plane.colorAt({x: 0, y: 0})).toEqual('black');
+      expect(plane.colorAt({x: 0, y: 1})).toEqual('black');
+      expect(plane.colorAt({x: 1, y: 1})).toEqual('white');
+      expect(plane.colorAt({x: 1, y: 0})).toEqual('white');
     });
   });
 });
