63a236d8e7dac5618bc1602f7c4344da1dae4584
Michel Grootjans <michel.grootjans@gmail.com>
2016-10-22 14:39:51 +0200

    initial setup

--- spec/langtons-ant-spec.js
+++ spec/langtons-ant-spec.js
@@ -1,5 +1,19 @@
 describe("Langton's ant", function(){
-  it('works', function(){
-    expect(ant).toBe(42);
+  var ant = null;
+  beforeEach(function() {
+    ant = new Ant();
+    plane = new Plane();
+  });
+
+  describe('initially', function() {
+    it('starts at (0,0)', function(){
+      expect(ant.position).toEqual({x: 0, y: 0});
+    });
+    it('starts facing north', function(){
+      expect(ant.direction).toEqual('north');
+    });
+    it('starts in a white plane', function() {
+      expect(plane.colorAt({x: 0, y: 0})).toEqual('white');
+    });
   });
 });
