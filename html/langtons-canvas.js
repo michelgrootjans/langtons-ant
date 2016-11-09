2524915b5c20d178888628aedf4c3a43e082f7f7
Michel Grootjans <michel.grootjans@gmail.com>
2016-11-09 08:38:06 +0100

    refactor

--- html/langtons-canvas.js
+++ html/langtons-canvas.js
@@ -1,21 +1,36 @@
 var canvas = function(){
   var canvas = document.getElementById('tutorial');
   var ctx = canvas.getContext('2d');
+  var cellSize = 8;
+
+  function drawRectangle(ctx, position, cellSize) {
+    ctx.fillRect (cellSize*position.x, cellSize*position.y, cellSize, cellSize);
+  }
+
+  function setPenToBlack(ctx) {
+    ctx.fillStyle = "rgb(50,50,50)";
+  }
+
+  function setPenToWhite(ctx) {
+    ctx.fillStyle = "rgb(200,200,200)";
+  }
+
   return {
     colorBlack: function(position) {
-      ctx.fillStyle = "rgb(200,0,0)";
-      ctx.fillRect (10*position.x, 10*position.y, 10, 10);
+      setPenToBlack(ctx);
+      drawRectangle(ctx, position, cellSize);
     },
+
     colorWhite: function(position) {
-      ctx.fillStyle = "rgb(255,255,255)";
-      ctx.fillRect (10*position.x, 10*position.y, 10, 10);
+      setPenToWhite(ctx);
+      drawRectangle(ctx, position, cellSize);
     }
   }
 }();
 
 var strategy = new XYStrategy();
 var plane = new Plane(strategy, canvas);
-var ant = new Ant(plane, strategy, {x: 40, y: 50});
+var ant = new Ant(plane, strategy, {x: 50, y: 100});
 
 for (var i = 0; i < 14000; i++) {
   setTimeout(function() {
